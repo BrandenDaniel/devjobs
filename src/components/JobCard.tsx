ef9f965138f90a67e20db2c9fb75b49f84ae58b6
Branden Ng <brandendanielng@gmail.com>
2023-08-30 22:07:09 +1000

    progress

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -4,7 +4,7 @@ import logo from "../assets/job-logo/scoot.svg";
 import { kumbh_sans } from "@/app/layout";
 
 type Props = {
-  id: number;
+  key: number;
   postedAt: string;
   contract: string;
   position: string;
@@ -17,9 +17,15 @@ type Props = {
 const JobCard = (props: Props) => {
   return (
     <>
-      <div className="jobCard" key={props.id}>
+      <div className="jobCard" key={props.key}>
         <div className="jobCard__inner">
-          <Image src={logo} alt="" style={{ backgroundColor: props.logoBg }} />
+          <Image
+            src={props.logo}
+            alt="company logo"
+            width={50}
+            height={50}
+            style={{ backgroundColor: props.logoBg }}
+          />
           <div className="jobCard__detail">
             <span>{props.postedAt} ago</span>
             <span>{props.contract}</span>
