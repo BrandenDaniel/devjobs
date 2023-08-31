bc0c1e195587b7cd32d315cc6598dc769c929061
Branden Ng <brandendanielng@gmail.com>
2023-08-31 21:17:01 +1000

    progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,26 +9,46 @@ import { kumbh_sans } from "@/app/layout";
 import jobData from "../data/data.json";
 
 const page = () => {
-  const [theme, setTheme] = useState<string>("light");
+  const [theme, setTheme] = useState("light");
+  const [titleQuery, setTitleQuery] = useState("");
+  const [locationQuery, setLocationQuery] = useState("");
+  const [isFullTime, setIsFullTime] = useState(false);
 
   return (
     <main className={kumbh_sans.className}>
       <Navbar theme={theme} setTheme={setTheme} />
-      <SearchFilter />
+      <SearchFilter
+        titleQuery={titleQuery}
+        setTitleQuery={setTitleQuery}
+        locationQuery={locationQuery}
+        setLocationQuery={setLocationQuery}
+        isFullTime={isFullTime}
+        setIsFullTime={setIsFullTime}
+      />
 
       <div className="container">
-        {jobData.map((job) => (
-          <JobCard
-            key={job.id}
-            postedAt={job.postedAt}
-            contract={job.contract}
-            position={job.position}
-            company={job.company}
-            location={job.location}
-            logoBg={job.logoBackground}
-            logo={job.logo}
-          />
-        ))}
+        {jobData.map((job) => {
+          if (
+            job.position.toLowerCase().includes(titleQuery.toLowerCase()) &&
+            job.location.toLowerCase().includes(locationQuery.toLowerCase())
+          ) {
+            if (isFullTime ? job.contract === "Full Time" : job.contract) {
+              return (
+                <JobCard
+                  key={job.id}
+                  postedAt={job.postedAt}
+                  contract={job.contract}
+                  position={job.position}
+                  company={job.company}
+                  location={job.location}
+                  logoBg={job.logoBackground}
+                  logo={job.logo}
+                />
+              );
+            }
+          }
+        })}
+        <p className="no-job">The job you're looking for does not exist.</p>
         <div className="loadMore">
           <button className={kumbh_sans.className}>Load More</button>
         </div>
--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -1,28 +1,55 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, FormEvent } from "react";
 import FilterIcon from "../assets/shared/icon-filter.svg";
 import SearchIcon from "../assets/shared/icon-search.svg";
 import Image from "next/image";
-import { kumbh_sans } from "@/app/layout";
 
 let sizeOnLoad: number = window.innerWidth;
 
-const SearchFilter = () => {
+type Props = {
+  titleQuery: string;
+  setTitleQuery: (query: string) => void;
+  locationQuery: string;
+  setLocationQuery: (query: string) => void;
+  isFullTime: boolean;
+  setIsFullTime: (arg: boolean) => void;
+};
+
+const SearchFilter = (props: Props) => {
   const [windowSize, setWindowSize] = useState<number>(sizeOnLoad);
   useEffect(() => {
+    sizeOnLoad = window.innerWidth;
     window.addEventListener("resize", () => {
       setWindowSize(window.innerWidth);
     });
   }, [windowSize]);
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const titleInput = e.currentTarget.querySelector(
+      'input[name="title"]'
+    ) as HTMLInputElement | null;
+
+    const locationInput = e.currentTarget.querySelector(
+      'input[name="location"]'
+    ) as HTMLInputElement | null;
+
+    const jobTypeCheck = e.currentTarget.querySelector(
+      'input[name="jobType"]'
+    ) as HTMLInputElement | null;
+
+    titleInput && props.setTitleQuery(titleInput.value);
+    locationInput && props.setLocationQuery(locationInput.value);
+    jobTypeCheck && props.setIsFullTime(jobTypeCheck.checked);
+  };
   return (
-    <form className="SearchFilter">
+    <form className="SearchFilter" onSubmit={handleSubmit}>
       <div className="SearchFilter__input-container">
         <div className="SearchFilter__title">
           <input
             type="text"
-            className={kumbh_sans.className}
+            name="title"
             placeholder={
               windowSize < 1200
                 ? "Filter by title…"
@@ -33,12 +60,17 @@ const SearchFilter = () => {
         <div className="SearchFilter__location">
           <input
             type="text"
-            className={kumbh_sans.className}
+            name="location"
             placeholder="Filter by location…"
           />
         </div>
         <div className="SearchFilter__job-type">
-          <input type="checkbox" id="jobType" />
+          <input
+            type="checkbox"
+            name="jobType"
+            id="jobType"
+            // checked={props.isFullTime ? true : false}
+          />
           <label htmlFor="jobType">
             Full Time{windowSize > 1200 ? " Only" : null}
           </label>
@@ -48,7 +80,7 @@ const SearchFilter = () => {
       <button className="SearchFilter__filter">
         <Image src={FilterIcon} alt="Filter" />
       </button>
-      <button className="SearchFilter__search">
+      <button className="SearchFilter__search" type="submit">
         {windowSize < 768 ? <Image src={SearchIcon} alt="Search" /> : "Search"}
       </button>
     </form>
